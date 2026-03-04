2d111d32c44bc1f84c7a2a80de88bfa2e1bc628a
Yiranubari <yiranubari4@gmail.com>
2026-03-05 00:33:51 +0100

    feat: update register method in AuthService to return user details

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -7,7 +7,9 @@ import { RegisterDTO } from "@/validators/authValidator";
 import { ConflictException } from "@/exceptions/app-exceptions";
 
 export class AuthService {
-  async register(data: RegisterDTO): Promise<void> {
+  async register(
+    data: RegisterDTO,
+  ): Promise<{ id: string; email: string; role: string }> {
     // Check if user with the same email already exists
     const existingUser = await prisma.user.findUnique({
       where: { email: data.email },
@@ -16,5 +18,25 @@ export class AuthService {
     if (existingUser) {
       throw new ConflictException("Email is already registered");
     }
+
+    // Hash the password before saving
+    const hashedPassword = await bcrypt.hash(data.password, 12);
+
+    // Create the user in the database
+    const user = await prisma.user.create({
+      data: {
+        email: data.email,
+        password: hashedPassword,
+        role: data.role,
+      },
+    });
+
+    const OTP = Math.floor(Math.random() * 1000000);
+
+    return {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    };
   }
 }
