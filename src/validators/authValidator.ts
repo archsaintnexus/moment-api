b2fb3b995b00fe5a34b6205d77928ee6a7347d6d
Yiranubari <yiranubari4@gmail.com>
2026-03-04 23:38:42 +0100

    feat: add default role to RegisterSchema in authValidator

--- src/validators/authValidator.ts
+++ src/validators/authValidator.ts
@@ -4,5 +4,7 @@ export const RegisterSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters long"),
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
-  role: z.enum(["CUSTOMER", "CREATIVE"]),
+  role: z.enum(["CUSTOMER", "CREATIVE"]).default("CUSTOMER"),
 });
+
+export type RegisterDTO = z.infer<typeof RegisterSchema>;
