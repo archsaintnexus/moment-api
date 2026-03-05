d496fe1fffcbd76b4ea3e817a8f04e0cd0490ea7
Yiranubari <yiranubari4@gmail.com>
2026-03-05 03:46:46 +0100

    feat: add route for OTP verification in authRoutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,6 +3,7 @@ import asyncHandler from "express-async-handler";
 import { AuthController } from "@/controllers/authController";
 import { RegisterSchema } from "@/validators/authValidator";
 import { LoginSchema } from "@/validators/authValidator";
+import { VerifyOtpSchema } from "@/validators/authValidator";
 import { validate } from "@/middlewares/validate.middleware";
 
 const router = Router();
@@ -20,4 +21,9 @@ router.post(
   asyncHandler(authController.login.bind(authController)),
 );
 
+router.post(
+  "/verify-otp",
+  validate(VerifyOtpSchema),
+  asyncHandler(authController.verifyOtp.bind(authController)),
+);
 export default router;
