4d77607f015383402801be8bb17fe4b8216dc784
Yiranubari <yiranubari4@gmail.com>
2026-03-06 13:35:55 +0100

    feat: add UserController with getMe method to retrieve user profile

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { UserService } from "@/services/user.service";
+import { LoggerBase } from "@/utils/logger";
+
+class UserController extends LoggerBase {
+  private userService = new UserService();
+
+  getMe = async (req: Request, res: Response): Promise<void> => {
+    const userId = req.user!.id;
+    const user = await this.userService.getMe(userId);
+    res.status(200).json({
+      success: true,
+      message: "User profile retrieved successfully",
+      data: user,
+    });
+  };
+}
+
+export const userController = new UserController();
+export default userController;
